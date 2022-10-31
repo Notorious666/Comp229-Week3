f8089cbe4bb26ffb7668402d031bc72398e94cbc
N666 <pahcomparan@gmail.com>
2022-10-31 11:34:25 -0400

    Added user and contact schema

--- models/user.js
+++ models/user.js
@@ -2,9 +2,57 @@ let mongoose = require('mongoose')
 
 //Creating the user model
 
-let userModel = mongoose.Schema({
-
-},
+let userModel = mongoose.Schema
+(
+    {
+username:
 {
-    collection: "users"
-})
+    type:String,
+    default:"",
+    trim: true,
+    required:"user name is required"
+},
+/* password:
+{
+    type:String,
+    default:"",
+    trim: true,
+    required:"password is required"
+}, */
+email:
+{
+    type:String,
+    default:"",
+    trim: true,
+    required:"email id is required"
+},
+displayName:
+{
+    type:String,
+    default:"",
+    trim: true,
+    required:"Display name is required"
+},
+created:
+{
+    type: Date,
+    default: Date.now
+},
+update:
+{
+    type: Date,
+    default: Date.now
+},
+    },
+
+    {
+      collection: "users"
+    }
+);
+
+// configure options for User Model
+
+let options = ({ missingPasswordError: "Wrong/ Missing Error"});
+User.plugin(passportlocalMongoose, options);
+
+module.exports.User = mongoose.model("User", User);
